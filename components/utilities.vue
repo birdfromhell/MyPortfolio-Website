596de1ab61142941a2b3bb7cef6450d3f83dd9b6
Ababil <95958484+birdfromhell@users.noreply.github.com>
2025-05-06 04:25:54 +0700

    Add utilities section to `utilities.vue` and horizontal scrolling animation to `main.css`
    * **`components/utilities.vue`**
      - Replace the existing flex container with a new flex-wrap container for utilities.
      - Add `UButton` component to display utilities in a grid layout.
      - Remove separate sections for "System" and "Software & Applications" utilities.
    
    * **`assets/css/main.css`**
      - Add keyframes for horizontal scrolling animation.
      - Add `.utilities-container` class for horizontal scrolling animation.

--- components/utilities.vue
+++ components/utilities.vue
@@ -13,19 +13,20 @@ import { utilities } from '~/data/utilities';
                 <h2 class="text-xl font-bold hover:cursor-pointer">Utilities</h2>
             </div>
         </a>
-        <div class="flex flex-col gap-3">
-            <div>
-                <h3 class="text-lg font-bold">System</h3>
-                <ul class="list-disc list-inside flex overflow-x-auto space-x-4">
-                    <li v-for="utility in utilities.system" :key="utility" class="flex-shrink-0">{{ utility }}</li>
-                </ul>
-            </div>
-            <div>
-                <h3 class="text-lg font-bold">Software & Applications</h3>
-                <ul class="list-disc list-inside flex overflow-x-auto space-x-4">
-                    <li v-for="utility in utilities.software" :key="utility" class="flex-shrink-0">{{ utility }}</li>
-                </ul>
-            </div>
+        <div class="flex flex-wrap gap-1.5">
+            <UButton
+                v-for="utility in [...utilities.system, ...utilities.software]"
+                :key="utility"
+                class="relative border border-stone-200 dark:border-stone-600 p-0.5 rounded-md w-24 h-24 sm:w-28 sm:h-28"
+                square
+                variant="soft"
+            >
+                <div class="flex flex-col items-center justify-evenly text-xs text-center w-full h-full">
+                    <p class="text-xs font-bold text-stone-800 dark:text-neutral-400">
+                        {{ utility }}
+                    </p>
+                </div>
+            </UButton>
         </div>
     </section>
 </template>
